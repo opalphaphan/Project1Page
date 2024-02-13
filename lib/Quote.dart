9a587a7c6b07958fb5596860c79e75758c9fbfca
Prince_eiei <sittiphon.ler@student.mahidol.ac.th>
2024-02-13 22:44:26 +0700

    Added Quote File

---
+++ lib/Quote.dart
@@ -0,0 +1,160 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Quote {
+  final String text;
+  final String author;
+
+  Quote({required this.text, required this.author});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: QuotePopup(),
+    );
+  }
+}
+
+class QuotePopup extends StatelessWidget {
+  final TextEditingController _quoteController = TextEditingController();
+  final TextEditingController _authorController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("your_background_image_path.jpg"), // Replace with your image path
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              color: Colors.blue.withOpacity(0.3),
+            ),
+          ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Text(
+              'DinoReads',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    AppBar(
+                      actions: [
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Add your Quote',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _quoteController,
+                      decoration: InputDecoration(
+                        labelText: 'Type your quote',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _authorController,
+                      decoration: InputDecoration(
+                        labelText: 'Type name of book or author',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      child: Text('Add'),
+                      onPressed: () {
+                        Quote quote = Quote(
+                          text: _quoteController.text,
+                          author: _authorController.text,
+                        );
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SuccessPage(quote: quote),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 50),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SuccessPage extends StatelessWidget {
+  final Quote quote;
+
+  SuccessPage({required this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Successfully Add Quote!')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Added Quote:', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text('"${quote.text}"', style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text('- ${quote.author}', style: TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+}
